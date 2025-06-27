e5380581f2358a96a21ce4b8c123239ccfc6c2c2
harikanagineni11 <harika.guna.1121@gmail.com>
2025-06-27 18:00:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let bn=document.querySelector(".rps");
+bn.addEventListener("click" ,function(){
+    window.open("https://harikanagineni11.github.io/my_rps/","_blank");
+});
+let bn2=document.querySelector(".profile_cards");
+bn2.addEventListener("click" ,function(){
+    window.open("https://harikanagineni11.github.io/profile-cards/","_blank");
+});
